d23d7ca0f35b6563aeca194261bcab2ad0b66724
Piotr <piotr.witczak.01@gmail.com>
2021-01-25 14:59:16 +0100

    Time interval, to do variable in Task model

--- ToDoList/views.py
+++ ToDoList/views.py
@@ -1,21 +1,19 @@
 from __future__ import unicode_literals
-from datetime import date
-from random import random
-from datetime import datetime
 
-from django.db.models import DateField
+from datetime import date, datetime
+
 # Create your views here.
 from django.forms import forms
-from django.shortcuts import render, redirect, get_object_or_404
-from django.utils.datastructures import MultiValueDictKeyError
-from django.template import RequestContext
+from django.shortcuts import render, redirect
+
 import ToDoList.models
 
 
 # Create your views here.
-
+temp = []
 
 def index(request):
+
     tasks = ToDoList.models.Task.objects.all()
     categories = ToDoList.models.Category.objects.all()
     if not categories:
@@ -37,23 +35,44 @@ def index(request):
 
             description = request.POST["description"]
 
+
+            date_format = "%Y/%m/%d"
+            time_interval = abs((datetime.strptime(date.today().strftime("%Y/%m/%d"), date_format) - datetime.strptime(deadline_date.replace('-', '/'), date_format)).days)
+            temp.append(time_interval)
+            for i in temp:
+                print(i)
+
+
+
             Todo = ToDoList.models.Task(task_name=task_name, description=description, deadline_date=deadline_date,
                                         category=ToDoList.models.Category.objects.get(category_name=category_name))
             Todo.save()
-            return redirect("/")
+            return render(request, "index.html", {"tasks": tasks, "categories": categories, "temp": temp})
 
         if "taskDelete" in request.POST:
             tasks_to_delete = request.POST.getlist('checkbox', False)
             for task_to_delete in tasks_to_delete:
                 todo = ToDoList.models.Task.objects.get(id=int(task_to_delete))
+                temp.clear()
                 todo.delete()
 
     return render(request, "index.html", {"tasks": tasks, "categories": categories})
+
 
 def create_data():
     names = ["Study", "Work", "Family", "Sport", "Personal", "Friends", "Shopping", "General", "Other", "Test"]
     for name in range(len(names)):
         ToDoList.models.Category.objects.create(category_name=names[name])
+
+
+def calculate_diff_date(created_at, deadline_date):
+    deadline_date_string = str(deadline_date)
+    y2, m2, d2 = [int(x) for x in deadline_date_string.split('-')]
+    d1 = date(y2, m2, d2)
+    created_at_string = str(created_at)
+    y2, m2, d2 = [int(x) for x in created_at_string.split('-')]
+    d2 = date(y2, m2, d2)
+    return d1 - d2
 
 
 # def Showcal(request):
