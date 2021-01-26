1e29aae25ddb8e542e0d551094967cfc4004b3ae
Piotr <piotr.witczak.01@gmail.com>
2021-01-26 14:22:05 +0100

    Routing

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -4,9 +4,15 @@ from django.conf.urls import url
 from django.urls import path
 
 from ToDoList import views
-from .views import SearchResultsView
+from .views import OrderByName, OrderByDays, OrderByDeadline, OrderByCategory
 
 urlpatterns = [
-    path('search/', SearchResultsView.as_view(), name='search_results'),
-    url('', views.index, name='index'),
+    path('search/', views.search, name='search_results'),
+    path('name/', OrderByName.as_view(), name='name'),
+    path('days/', OrderByDays.as_view(), name='days'),
+    path('deadline/', OrderByDeadline.as_view(), name='deadline'),
+    path('category/', OrderByCategory.as_view(), name='category'),
+
+
+    path('', views.index, name='index'),
 ]
--- ToDoList/views.py
+++ ToDoList/views.py
@@ -2,28 +2,67 @@ from __future__ import unicode_literals
 
 from datetime import date, datetime
 
-from django.db.models import Q  # new
-# Create your views here.
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+from django.db.models.functions import Lower
 from django.forms import forms
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
-
+from django.urls import resolve
 from ToDoList.models import *
 
 
 # Create your views here.
+def search(request):
+    return Task.objects.filter(Q(task_name__icontains=request.GET.get('q')))
 
-class SearchResultsView(ListView):
+class OrderByName(ListView):
+    model = Task
+    template_name = 'index.html'
+    def get_queryset(self):
+        current_url = resolve(self.request.path_info).url_name
+        print(current_url)
+        if "name" in str(current_url):
+            print("Haha")
+            return Task.objects.order_by('task_name').reverse()
+        return Task.objects.order_by('task_name')
+
+
+class OrderByDays(ListView):
     model = Task
     template_name = 'index.html'
 
-    def get_queryset(self):  # new
-        query = self.request.GET.get('q')
-        object_list = Task.objects.filter(Q(task_name__icontains=query))
-        return object_list
+    def get_queryset(self):
+        current_url = resolve(self.request.path_info).url_name
+        if "days" in str(current_url):
+            return Task.objects.order_by('time_interval').reverse()
+        return Task.objects.order_by('time_interval')
+
+class OrderByDeadline(ListView):
+    model = Task
+    template_name = 'index.html'
+
+    def get_queryset(self):
+        current_url = resolve(self.request.path_info).url_name
+        if "deadline" in str(current_url):
+            return Task.objects.order_by('deadline_date').reverse()
+        return Task.objects.order_by('deadline_date')
+
+
+class OrderByCategory(ListView):
+    model = Task
+    template_name = 'index.html'
+
+    def get_queryset(self):
+        current_url = resolve(self.request.path_info).url_name
+        if "category" in str(current_url):
+            return Task.objects.order_by('category').reverse()
+        return Task.objects.order_by('category')
+
 
 
 def index(request):
+    template_name = 'index.html'
     tasks = Task.objects.all()
     categories = Category.objects.all()
     if not categories:
@@ -34,22 +73,14 @@ def index(request):
             task_name = request.POST["task_name"]
             category_name = request.POST["category_name"]
             deadline_date = str(request.POST["deadline_date"])
-
-            # try:
-            #     deadline_date = str(request.POST["deadline_date"])
-            # except MultiValueDictKeyError:
-            #     deadline_date = DateField(auto_now=True)
             y2, m2, d2 = [int(x) for x in deadline_date.split('-')]
             if date(y2, m2, d2) < date.today():
                 raise forms.ValidationError(message='Deadline date cannot be in past!')
-
             description = request.POST["description"]
-
             date_format = "%Y/%m/%d"
             time_interval = int(abs((datetime.strptime(date.today().strftime("%Y/%m/%d"),
                                                        date_format) - datetime.strptime(deadline_date.replace('-', '/'),
                                                                                         date_format)).days))
-
             Todo = Task(task_name=task_name, description=description, deadline_date=deadline_date,
                         time_interval=time_interval,
                         category=Category.objects.get(category_name=category_name))
@@ -63,6 +94,28 @@ def index(request):
                 todo.delete()
 
     return render(request, "index.html", {"tasks": tasks, "categories": categories})
+
+
+def company_index(request):
+    order_by = request.GET.get('order_by')
+    direction = request.GET.get('direction')
+    ordering = Lower(order_by)
+    if direction == 'desc':
+        ordering = '-{}'.format(ordering)
+    companies = Task.objects.all().order_by(ordering)
+
+    paginator = Paginator(companies, 10)
+    page = request.GET.get('page')
+    try:
+        all_companies = paginator.page(page)
+    except PageNotAnInteger:
+        all_companies = paginator.page(1)
+    except EmptyPage:
+        all_companies = paginator.page(paginator.num_pages)
+
+    return render(request, 'index.html',
+                  {'tasks': all_companies,
+                   'order_by': order_by, 'direction': direction})
 
 
 def create_data():
